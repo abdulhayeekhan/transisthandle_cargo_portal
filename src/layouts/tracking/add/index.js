34c3d416ace3c8803f87214786435a68dd0315cd
Dev Ninja <codeveloper101@gmail.com>
2025-07-21 20:17:53 +0500

    tracking updated

--- src/layouts/tracking/add/index.js
+++ src/layouts/tracking/add/index.js
@@ -16,6 +16,8 @@ import {
   Typography,
   Icon,
   FormLabel,
+  Card,
+  CardContent,
 } from "@mui/material";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import MDBox from "components/MDBox";
@@ -40,14 +42,14 @@ function AddTracking() {
   });
 
   const parts = formatter.formatToParts(new Date());
-  const createdAt = `${parts.find((p) => p.type === "year").value}-${
-    parts.find((p) => p.type === "month").value
-  }-${parts.find((p) => p.type === "day").value}`;
+  const createdAt = `${parts.find((p) => p.type === "year").value}-${parts.find((p) => p.type === "month").value
+    }-${parts.find((p) => p.type === "day").value}`;
   const [trackingInfo, setTrackingInfo] = useState({
     CNIC: "",
     ContactNo: "",
     Email: "",
     PostalCode: "",
+    Address: "",
     CityId: 1,
     CreatedBy: createdBy,
     ModifiedBy: createdBy,
@@ -55,6 +57,13 @@ function AddTracking() {
     WeightUnit: "Kg",
     Description: "",
     BookingCityId: "",
+    ConsignAddress: "",
+    ConsignCountryId: "",
+    ConsignZipCode: "",
+    ConsignContactNo: "",
+    FreightAmountPKR: 0,
+    ValuePKR: 0,
+    NoOfPcs:0
   });
   console.log("trackingInfo:", trackingInfo);
   const [IsForUK, setIsForUK] = useState(true);
@@ -82,7 +91,9 @@ function AddTracking() {
   };
 
   const [saveButton, setSaveButton] = useState(false);
+  const [isPost,setIsPost] = useState(false)
   const handleSaveCompany = async (e) => {
+    setIsPost(true)
     e.preventDefault();
     const finalBody = {
       ...trackingInfo,
@@ -96,160 +107,295 @@ function AddTracking() {
       {/* <DashboardNavbar /> */}
       <form onSubmit={(e) => handleSaveCompany(e)}>
         <MDBox
-          sx={{
-            height: "65vh", // Set a fixed height for the scrollable area
-            overflowY: "auto", // Enable vertical scrolling
-            padding: 2,
-            marginY: 2,
-          }}
         >
-          <Grid container spacing={2}>
-            {/* Top Section */}
-            <Grid item xs={12}>
-              <Typography>Cutomer Info</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                label="FirstName"
-                name="FirstName"
-                value={trackingInfo?.FirstName}
-                onChange={(e) => handleChangeData(e)}
-                fullWidth
-                required
-                size="small"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                label="LastName"
-                name="LastName"
-                value={trackingInfo?.LastName}
-                onChange={(e) => handleChangeData(e)}
-                fullWidth
-                required
-                size="small"
-              />
-            </Grid>
-            <Grid display="flex" item xs={12} gap={2} sm={6}>
-              <TextField
-                label="Contact No"
-                name="ContactNo"
-                value={trackingInfo?.ContactNo}
-                onChange={(e) => handleChangeData(e)}
-                size="small"
-                fullWidth
-              />
-            </Grid>
-            <Grid display="flex" item xs={12} gap={2} sm={6}>
-              <TextField
-                label="Email"
-                name="Email"
-                value={trackingInfo?.Email}
-                onChange={(e) => handleChangeData(e)}
-                size="small"
-                fullWidth
-              />
-            </Grid>
+          <Card style={{ backgroundColor: '#fff' }}>
+            <CardContent>
+              <Grid container spacing={2}>
+                {/* Top Section */}
 
-            <Grid item xs={12} sm={6}>
-              <InputMask
-                mask="99999-9999999-9"
-                name="CNIC"
-                value={trackingInfo.CNIC}
-                onChange={(e) => handleChangeData(e)}
-              >
-                {(inputProps) => (
+
+                <Grid item xs={6}
+                  sx={{
+                    border: '1px solid #ccc',
+                    borderRadius: '8px',
+                    padding: 2, // 16px padding inside the box
+                    backgroundColor: '#f8fde3',
+                    marginTop: 3
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <Typography>Shipper Informarion</Typography>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label="Shipper FirstName"
+                        name="FirstName"
+                        value={trackingInfo?.FirstName}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label="Shipper LastName"
+                        name="LastName"
+                        value={trackingInfo?.LastName}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+
+                    <Grid display="flex" item xs={12} gap={2} sm={12}>
+                      <TextField
+                        label="Address"
+                        name="Address"
+                        value={trackingInfo?.Address}
+                        onChange={(e) => handleChangeData(e)}
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                      <InputMask
+                        mask="99999-9999999-9"
+                        name="CNIC"
+                        value={trackingInfo.CNIC}
+                        onChange={(e) => handleChangeData(e)}
+                      >
+                        {(inputProps) => (
+                          <TextField
+                            size="small"
+                            name="CNIC"
+                            label="CNIC No"
+                            {...inputProps}
+                            placeholder="XXXXX-XXXXXXX-X"
+                            fullWidth
+                            variant="outlined"
+                          />
+                        )}
+                      </InputMask>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        size="small"
+                        label="Postal code"
+                        name="PostalCode"
+                        onChange={(e) => handleChangeData(e)}
+                        value={trackingInfo?.PostalCode}
+                        fullWidth
+                      />
+                    </Grid>
+
+                    <Grid display="flex" item xs={12} gap={2} sm={6}>
+                      <TextField
+                        label="Contact No"
+                        name="ContactNo"
+                        value={trackingInfo?.ContactNo}
+                        onChange={(e) => handleChangeData(e)}
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+
+
+
+                    <Grid display="flex" item xs={12} gap={2} sm={6}>
+                      <TextField
+                        label="Email"
+                        name="Email"
+                        value={trackingInfo?.Email}
+                        onChange={(e) => handleChangeData(e)}
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={6}
+                  sx={{
+                    border: '1px solid #ccc',
+                    borderRadius: '8px',
+                    padding: 2, // 16px padding inside the box
+                    backgroundColor: '#fde7e3',
+                    marginTop: 3
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <Typography>Consignee Informarion</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                      <TextField
+                        label="Consignee Full Name"
+                        name="ConsignName"
+                        value={trackingInfo?.ConsignName}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                      <TextField
+                        label="Address"
+                        name="ConsignAddress"
+                        value={trackingInfo?.ConsignAddress}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                      <TextField
+                        label="Zip Code"
+                        name="ConsignZipCode"
+                        value={trackingInfo?.ConsignZipCode}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label="Contact No"
+                        name="ConsignContactNo"
+                        value={trackingInfo?.ConsignContactNo}
+                        onChange={(e) => handleChangeData(e)}
+                        fullWidth
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={12} sm={3}>
                   <TextField
+                    label="Freight Amount (PKR)"
+                    name="FreightAmountPKR"
+                    value={trackingInfo?.FreightAmountPKR}
+                    onChange={(e) => handleChangeData(e)}
                     size="small"
-                    name="CNIC"
-                    label="CNIC No"
-                    {...inputProps}
-                    placeholder="XXXXX-XXXXXXX-X"
+                    type="number"
+                    required
                     fullWidth
-                    variant="outlined"
                   />
-                )}
-              </InputMask>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                label="Weight (KG)"
-                name="Weight"
-                value={trackingInfo?.Weight}
-                onChange={(e) => handleChangeData(e)}
-                size="small"
-                type="number"
-                required
-                fullWidth
-              />
-            </Grid>
+                </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <TextField
-                size="small"
-                label="Postal code"
-                name="PostalCode"
-                onChange={(e) => handleChangeData(e)}
-                value={trackingInfo?.PostalCode}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Autocomplete
-                fullWidth
-                name="creditAccountId"
-                size="small"
-                options={bookingCities}
-                value={cityValue} // Bind the selected value (object with `id`, `label`, `code`)
-                onChange={handleCityChange}
-                getOptionLabel={(option) => option.name} // Combine `label` and `code` for display
-                renderInput={(params) => (
+                <Grid item xs={12} sm={3}>
                   <TextField
+                    label="Value (PKR)"
+                    name="ValuePKR"
+                    value={trackingInfo?.ValuePKR}
+                    onChange={(e) => handleChangeData(e)}
+                    size="small"
+                    type="number"
+                    required
+                    fullWidth
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    label="Weight (KG)"
+                    name="Weight"
+                    value={trackingInfo?.Weight}
+                    onChange={(e) => handleChangeData(e)}
+                    size="small"
+                    type="number"
+                    required
+                    fullWidth
+                  />
+                </Grid>
+
+                
+                <Grid item xs={12} sm={3}>
+                  <Autocomplete
+                    fullWidth
                     name="creditAccountId"
                     size="small"
-                    {...params}
-                    label="Order Booking City"
+                    options={bookingCities}
+                    value={cityValue} // Bind the selected value (object with `id`, `label`, `code`)
+                    onChange={handleCityChange}
+                    getOptionLabel={(option) => option.name} // Combine `label` and `code` for display
+                    renderInput={(params) => (
+                      <TextField
+                        name="creditAccountId"
+                        size="small"
+                        {...params}
+                        label="Order Booking City"
+                      />
+                    )}
                   />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                label="Description"
-                name="Description"
-                value={trackingInfo?.Description}
-                onChange={(e) => handleChangeData(e)}
-                fullWidth
-                size="small"
-              />
-            </Grid>
+                </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <Box display="flex" flexDirection="column" gap={4}>
-                <FormControl>
-                  <FormLabel>This Tracking</FormLabel>
-                  <RadioGroup
-                    row
-                    value={IsForUK}
-                    onChange={(e) => setIsForUK(e.target.value)}
-                    display="flex"
-                    gap={4}
-                  >
-                    <FormControlLabel value={true} control={<Radio />} label="For UK" />
-                    <FormControlLabel value={false} control={<Radio />} label="For USA" />
-                  </RadioGroup>
-                </FormControl>
-              </Box>
-            </Grid>
-          </Grid>
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    label="No Of Pcs"
+                    name="NoOfPcs"
+                    value={trackingInfo?.NoOfPcs}
+                    onChange={(e) => handleChangeData(e)}
+                    size="small"
+                    type="number"
+                    required
+                    fullWidth
+                  />
+                </Grid>
+
+
+                
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    label="Description"
+                    name="Description"
+                    value={trackingInfo?.Description}
+                    onChange={(e) => handleChangeData(e)}
+                    fullWidth
+                    size="small"
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <Box display="flex" flexDirection="column" gap={4}>
+                    <FormControl>
+                      <FormLabel>This Tracking</FormLabel>
+                      <RadioGroup
+                        row
+                        value={IsForUK}
+                        onChange={(e) => setIsForUK(e.target.value)}
+                        display="flex"
+                        gap={4}
+                      >
+                        <FormControlLabel value={true} control={<Radio />} label="For UK" />
+                        <FormControlLabel value={false} control={<Radio />} label="For USA" />
+                      </RadioGroup>
+                    </FormControl>
+                  </Box>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
         </MDBox>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} sx={{marginTop:2}}>
           <Grid item xs={12}>
             <Box display="flex" style={{ justifyContent: "center" }} gap={2} mb={2}>
               <Button
                 variant="contained"
                 style={{ color: "#fff" }}
-                disabled={saveButton}
+                disabled={isPost}
                 type="submit"
               >
                 <Icon fontSize="large">save</Icon>
@@ -258,6 +404,7 @@ function AddTracking() {
 
               <Button
                 variant="outlined"
+                disabled={isPost}
                 onClick={(e) => navigate("/tracking")}
                 style={{ color: "grey" }}
               >
